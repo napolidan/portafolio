099290c25ee4fb95e3b758e6f0817171bd5a70f0
napolidan <danielnapnapolidan@gmail.com>
2023-11-27 15:32:10 -0500

    Listo para deploy

--- src/routes/Curriculum.jsx
+++ src/routes/Curriculum.jsx
@@ -6,13 +6,15 @@ const Curriculum = () => {
         <>
             <Menu />
             <div className="container curriculum d-flex flex-column">
-                <div className="d-flex gap-4">
-                    <img className="cv" src="./images/cv.jpg" />
-                    <span className="yellow-square w-50 d-flex justify-content-center">
-                        <h1 className="basement no-visible-responsive text-end position-absolute">Curriculum<br></br>Vitae</h1>
-                        <h1 className="basement visible-responsive">CV</h1>
-                        <img className="w-100 opacity-0" src="./images/design1.jpg" />
-                    </span>
+
+                <div className="w-100 gap-4 d-flex">
+                    <div className="w-50">
+                        <img className="w-100" src="./images/cv.jpg" />
+                    </div>
+                    <div className="yellow-square-curriculum w-50">
+                        <h1 className="basement no-visible-responsive text-end">Curriculum<br />Vitae</h1>
+                        <h1 className="basement visible-responsive text-end">CV</h1>
+                    </div>
                 </div>
 
                 <h3 className="basement mt-auto pt-5">2023</h3>

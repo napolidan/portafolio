1f7481b349262bcf877a96c2ed1a591ed28b9bc3
napolidan <danielnapnapolidan@gmail.com>
2024-03-25 06:54:37 -0500

    added some project links

--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -14,87 +14,95 @@ const Projects = () => {
                 <br />
                 <br />
                 <div className="d-flex projects-flex flex-wrap justify-content-center gap-5">
-                    <div className="d-flex flex-column gap-3 align-items-center">
+                    <a href="https://napolidan.github.io/carousel/" target="_blank" className="d-flex flex-column gap-3 align-items-center text-decoration-none">
                         <div className="image-cover">
                             <div>
                                 <img src="./images/proj1.png" alt="" className="project" />
                             </div>
                         </div>
                         <h1 className="basement-subtitle yellow-text text-center pt-3">Solociencia</h1>
-                    </div>
-                    <div className="d-flex flex-column gap-3 align-items-center">
+                    </a>
+                    <a href="https://napolidan.github.io/carousel/" target="_blank" className="d-flex flex-column gap-3 align-items-center text-decoration-none">
                         <div className="image-cover">
                             <div>
                                 <img src="./images/proj2.png" alt="" className="project" />
                             </div>
                         </div>
                         <h1 className="basement-subtitle yellow-text text-center pt-3">CSS carousel</h1>
-                    </div>
-                    <div className="d-flex flex-column gap-3 align-items-center">
+                    </a>
+                    <a href="https://napolidan.github.io/carousel/" target="_blank" className="d-flex flex-column gap-3 align-items-center text-decoration-none">
                         <div className="image-cover">
                             <div>
                                 <img src="./images/proj1.png" alt="" className="project" />
                             </div>
                         </div>
                         <h1 className="basement-subtitle yellow-text text-center pt-3">React Navbar</h1>
-                    </div>
-                    <div className="d-flex flex-column gap-3 align-items-center">
+                    </a>
+                    <a href="https://napolidan.github.io/carousel/" target="_blank" className="d-flex flex-column gap-3 align-items-center text-decoration-none">
                         <div className="image-cover">
                             <div>
                                 <img src="./images/proj4.png" alt="" className="project" />
                             </div>
                         </div>
                         <h1 className="basement-subtitle yellow-text text-center pt-3">Framer motion modal</h1>
-                    </div>
-                    <div className="d-flex flex-column gap-3 align-items-center">
+                    </a>
+                    <a href="https://napolidan.github.io/fmotion-basic-demo/" target="_blank" className="d-flex flex-column gap-3 align-items-center text-decoration-none">
                         <div className="image-cover">
                             <div>
                                 <img src="./images/proj5.png" alt="" className="project" />
                             </div>
                         </div>
                         <h1 className="basement-subtitle yellow-text text-center pt-3">Framer motion demo</h1>
-                    </div>
-                    <div className="d-flex flex-column gap-3 align-items-center">
+                    </a>
+                    <a href="https://napolidan.github.io/carousel/" target="_blank" className="d-flex flex-column gap-3 align-items-center text-decoration-none">
                         <div className="image-cover">
                             <div>
                                 <img src="./images/proj6.png" alt="" className="project" />
                             </div>
                         </div>
                         <h1 className="basement-subtitle yellow-text text-center pt-3">Framer motion animation</h1>
-                    </div>
-                    <div className="d-flex flex-column gap-3 align-items-center">
+                    </a>
+                    <a href="https://napolidan.github.io/fmotion-portfolio-demo/" target="_blank" className="d-flex flex-column gap-3 align-items-center text-decoration-none">
                         <div className="image-cover">
                             <div>
                                 <img src="./images/proj7.png" alt="" className="project" />
                             </div>
                         </div>
-                        <h1 className="basement-subtitle yellow-text text-center pt-3">Mixblend demo</h1>
-                    </div>
-                    <div className="d-flex flex-column gap-3 align-items-center">
+                        <h1 className="basement-subtitle yellow-text text-center pt-3">Framer motion portfolio</h1>
+                    </a>
+                    <a href="https://napolidan.github.io/carousel/" target="_blank" className="d-flex flex-column gap-3 align-items-center text-decoration-none">
                         <div className="image-cover">
                             <div>
                                 <img src="./images/proj8.png" alt="" className="project" />
                             </div>
                         </div>
-                        <h1 className="basement-subtitle yellow-text text-center pt-3">Page transition</h1>
-                    </div>
-                    <div className="d-flex flex-column gap-3 align-items-center">
+                        <h1 className="basement-subtitle yellow-text text-center pt-3">Mixblend demo</h1>
+                    </a>
+                    <a href="https://napolidan.github.io/carousel/" target="_blank" className="d-flex flex-column gap-3 align-items-center text-decoration-none">
                         <div className="image-cover">
                             <div>
                                 <img src="./images/proj9.png" alt="" className="project" />
                             </div>
                         </div>
-                        <h1 className="basement-subtitle yellow-text text-center pt-3">Music player</h1>
-                    </div>
-                    <div className="d-flex flex-column gap-3 align-items-center">
+                        <h1 className="basement-subtitle yellow-text text-center pt-3">Page transition</h1>
+                    </a>
+                    <a href="https://napolidan.github.io/carousel/" target="_blank" className="d-flex flex-column gap-3 align-items-center text-decoration-none">
                         <div className="image-cover">
                             <div>
                                 <img src="./images/proj10.png" alt="" className="project" />
                             </div>
                         </div>
+                        <h1 className="basement-subtitle yellow-text text-center pt-3">Music player</h1>
+                    </a>
+                    <a href="https://napolidan.github.io/carousel/" target="_blank" className="d-flex flex-column gap-3 align-items-center text-decoration-none">
+                        <div className="image-cover">
+                            <div>
+                                <img src="./images/proj11.png" alt="" className="project" />
+                            </div>
+                        </div>
                         <h1 className="basement-subtitle yellow-text text-center pt-3">CSS-only Navbar</h1>
-                    </div>
-                    
+                    </a>
+
 
                 </div>
             </div>
